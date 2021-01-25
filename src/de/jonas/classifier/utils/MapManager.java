b6745fbd5ca5e6512a5ad3da136efaae12fac3ed
BuildTools <unconfigured@null.spigotmc.org>
2021-01-25 20:10:43 +0100

    Update to MapManager.java

--- src/de/jonas/classifier/utils/MapManager.java
+++ src/de/jonas/classifier/utils/MapManager.java
@@ -5,65 +5,42 @@ import java.util.*;
 
 public class MapManager {
 
-    public Map<String, Integer> sortByComparator(Map<String, Integer> unsortMap, final boolean order)
-    {
+    public Map<String, Integer> sortByComparator(Map<String, Integer> unsortMap, final boolean order) {
         List<Map.Entry<String, Integer>> list = new LinkedList<Map.Entry<String, Integer>>(unsortMap.entrySet());
         // Sorting the list based on values
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>()
-        {
-            public int compare(Map.Entry<String, Integer> o1,
-                               Map.Entry<String, Integer> o2)
-            {
-                if (order)
-                {
-                    return o1.getValue().compareTo(o2.getValue());
-                }
-                else
-                {
-                    return o2.getValue().compareTo(o1.getValue());
-
-                }
+        Collections.sort(list, (o1, o2) -> {
+            if (order) {
+                return o1.getValue().compareTo(o2.getValue());
+            } else {
+                return o2.getValue().compareTo(o1.getValue());
             }
         });
 
         // Maintaining insertion order with the help of LinkedList
-        Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
-        for (Map.Entry<String, Integer> entry : list)
-        {
+        Map<String, Integer> sortedMap = new LinkedHashMap<>();
+        for (Map.Entry<String, Integer> entry : list) {
             sortedMap.put(entry.getKey(), entry.getValue());
         }
 
         return sortedMap;
     }
 
-    public Map<BufferedImage, Double> sortByComparatorFinal(Map<BufferedImage, Double> unsortMap, final boolean order)
-    {
-        List<Map.Entry<BufferedImage, Double>> list = new LinkedList<Map.Entry<BufferedImage, Double>>(unsortMap.entrySet());
+    public Map<BufferedImage, Double> sortByComparatorFinal(Map<BufferedImage, Double> unsortMap, final boolean order) {
+        List<Map.Entry<BufferedImage, Double>> list = new LinkedList<>(unsortMap.entrySet());
         // Sorting the list based on values
-        Collections.sort(list, new Comparator<Map.Entry<BufferedImage, Double>>()
-        {
-            public int compare(Map.Entry<BufferedImage, Double> o1,
-                               Map.Entry<BufferedImage, Double> o2)
-            {
-                if (order)
-                {
-                    return o1.getValue().compareTo(o2.getValue());
-                }
-                else
-                {
-                    return o2.getValue().compareTo(o1.getValue());
-
-                }
+        Collections.sort(list, (o1, o2) -> {
+            if (order) {
+                return o1.getValue().compareTo(o2.getValue());
+            } else {
+                return o2.getValue().compareTo(o1.getValue());
             }
         });
 
         // Maintaining insertion order with the help of LinkedList
-        Map<BufferedImage, Double> sortedMap = new LinkedHashMap<BufferedImage, Double>();
-        for (Map.Entry<BufferedImage, Double> entry : list)
-        {
+        Map<BufferedImage, Double> sortedMap = new LinkedHashMap<>();
+        for (Map.Entry<BufferedImage, Double> entry : list) {
             sortedMap.put(entry.getKey(), entry.getValue());
         }
-
         return sortedMap;
     }
 
